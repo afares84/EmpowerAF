60061d5e0654443d1a873429a268c1aad4a16072
Fares <afares@stellar-pcs.com>
2023-03-28 14:25:00 +0200

    Projektdateien hinzufügen.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerAF_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerAF_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("F6E1228E-07E5-4711-AA26-6C44DD2F39BB")]
+[assembly: AssemblyVersion("1.0.0.0")]
